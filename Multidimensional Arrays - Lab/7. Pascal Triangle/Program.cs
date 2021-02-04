f026c594db47bd893c4607c362c916f0b8084d55
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-04 18:32:46 +0000

    Add multidimentional arrays lab

---
+++ Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            long[][] matrix = new long[numberOfRows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new long[row + 1];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (col == 0 || col == row)
+                    {
+                        matrix[row][col] = 1;
+                    }
+                    else
+                    {
+                        matrix[row][col] = matrix[row - 1][col] + matrix[row - 1][col - 1];
+                    }
+                }
+            }
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                Console.WriteLine(String.Join(" ", matrix[row]));
+            }
+        }
+    }
+}
