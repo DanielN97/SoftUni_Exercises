7b4a2bf0a26073750d559f2d8921875e03b20d92
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-08 01:40:38 +0000

    Add exercise 4

---
+++ Multidimensional Arrays - Exercise/4. Matrix Shuffling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeOfMatrix = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[sizeOfMatrix[0], sizeOfMatrix[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "swap" && command.Length == 5 && int.Parse(command[1]) >= 0 && int.Parse(command[1]) <= matrix.GetLength(0) && int.Parse(command[2]) >= 0 && int.Parse(command[2]) <= matrix.GetLength(1) && int.Parse(command[3]) >= 0 && int.Parse(command[3]) <= matrix.GetLength(0) && int.Parse(command[4]) >= 0 && int.Parse(command[4]) <= matrix.GetLength(1))
+                {
+                    int temp = matrix[int.Parse(command[1]), int.Parse(command[2])];
+                    matrix[int.Parse(command[1]), int.Parse(command[2])] = matrix[int.Parse(command[3]), int.Parse(command[4])];
+                    matrix[int.Parse(command[3]), int.Parse(command[4])] = temp;
+
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            Console.Write(matrix[row, col] + " ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
