feadab98f4114a0b254ed791c6474a0f279eba35
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-09 05:00:39 +0000

    Add exercise 6

---
+++ Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            float[][] jaggedArray = new float[n][];
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                jaggedArray[row] = new float[rowData.Length];
+
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    jaggedArray[row][col] = rowData[col];
+                }
+            }
+
+            for (int row = 0; row < jaggedArray.Length - 1; row++)
+            {
+                if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] *= 2;
+                    }
+                    for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+                    {
+                        jaggedArray[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+                    {
+                        jaggedArray[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "End")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                float num = float.Parse(command[3]);
+
+                if (command[0] == "Add")
+                {
+                    if (row >= 0 && col >= 0 && row < jaggedArray.Length && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] += num;
+                    }
+                }
+                else if (command[0] == "Subtract")
+                {
+                    if (row >= 0 && col >= 0 && row < jaggedArray.Length && col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] -= num;
+                    }
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write(jaggedArray[row][col]  + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
