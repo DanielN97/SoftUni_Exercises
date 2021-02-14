3c353384b2a1cbce4fa8c5aa4508cc74e2add318
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-14 16:59:10 +0000

    Add exercise 5

---
+++ Sets and Dictionaries Advanced - Lab/05. Record Unique Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dataBase = new HashSet<string>();
+
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string name = Console.ReadLine();
+
+                dataBase.Add(name);
+            }
+
+            foreach (string name in dataBase)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
