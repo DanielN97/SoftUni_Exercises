8befb0b19ec052d13cfed793e00bcfeb4a24a87a
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-14 17:08:04 +0000

    Add exercise 6

---
+++ Sets and Dictionaries Advanced - Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dataBase = new HashSet<string>();
+
+            string[] command = Console.ReadLine().Split(", ");
+
+            while (command[0] != "END")
+            {
+                string direction = command[0];
+                string registrationNumber = command[1];
+
+                if (direction == "IN")
+                {
+                    dataBase.Add(registrationNumber);
+                }
+                else if (direction == "OUT" && dataBase.Contains(registrationNumber))
+                {
+                    dataBase.Remove(registrationNumber);
+                }
+
+                command = Console.ReadLine().Split(", ");
+            }
+
+            if (dataBase.Count != 0)
+            {
+                foreach (string number in dataBase)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
