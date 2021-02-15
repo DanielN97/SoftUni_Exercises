521f750f391055d48a3364f34a4fd5f10ef4406d
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-15 00:04:29 +0000

    Add Sets and Dictionaties Advanced folder and add exercise 1

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            foreach (string name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
