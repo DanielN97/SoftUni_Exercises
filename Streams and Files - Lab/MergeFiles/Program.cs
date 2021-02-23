277d4bca3ab04ab784d49c8ae962d0b74c1ed8a4
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-23 22:04:30 +0000

    Add WordCount exercise

---
+++ Streams and Files - Lab/MergeFiles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader firstReader = new StreamReader(@"../../../FileOne.txt"))
+            {
+                using (StreamReader secondReader = new StreamReader(@"../../../FileTwo.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter(@"../../../Output.txt"))
+                    {
+                        int counter = 0;
+
+                        while (firstReader.Peek() > 0 || secondReader.Peek() > 0)
+                        {
+                            if (counter % 2 == 0)
+                            {
+                                writer.WriteLine(firstReader.ReadLine());
+                            }
+                            else
+                            {
+                                writer.WriteLine(secondReader.ReadLine());
+                            }
+
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
