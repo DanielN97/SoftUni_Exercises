9728635973212f1b0b2f663a6203989556957725
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-23 12:57:42 +0000

    Add Streams and Files - Lab folder and exercise 1

---
+++ Streams and Files - Lab/OddLines/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C: \Users\danie\Desktop\New folder\Resources\01.Odd Lines"))
+            {
+                int row = 0;
+                string currentRow = reader.ReadLine();
+
+                while (currentRow != null)
+                {
+                    if (row % 2 == 1)
+                    {
+                        Console.WriteLine(currentRow);
+                    }
+
+                    currentRow = reader.ReadLine();
+                    row++;
+                }
+            }
+        }
+    }
+}
