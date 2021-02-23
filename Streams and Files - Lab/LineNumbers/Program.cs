9b0c33a7a145ee04d1dd3a4c0cb8e5793e0346a9
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-23 15:51:20 +0000

    Add exercise 2

---
+++ Streams and Files - Lab/LineNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int counter = 1;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine(line.Insert(0, $"{counter}. "));
+
+                        counter++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
