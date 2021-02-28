c2d45a51658a61d08303c71b0a122ffa3056fb9a
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-28 10:43:04 +0000

    Add Streams, Files and Dictionaries - Exercise folder and EvenLines exercise

---
+++ Streams, Files and Dictionaries - Exercise/EvenLines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                int counter = 1;
+
+                while (!reader.EndOfStream)
+                {
+                    string currentLine = reader.ReadLine();
+                    counter++;
+
+                    if (counter % 2 == 0)
+                    {
+                        Regex regex = new Regex(@"[-,.!?]");
+
+                        string replacedLine =  regex.Replace(currentLine, "@");
+
+                        string[] words = replacedLine.Split();
+                        string[] reversedLine = words.Reverse().ToArray(); ;
+
+                        Console.WriteLine(String.Join(" ", reversedLine));
+                    }
+                }
+            }
+        }
+    }
+}
