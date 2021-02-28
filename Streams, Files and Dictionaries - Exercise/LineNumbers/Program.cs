8d01ae9666d202e956e650c56955bffa8945d2aa
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-02-28 12:35:24 +0000

    Add LineNumbers exercise

---
+++ Streams, Files and Dictionaries - Exercise/LineNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines = File.ReadAllLines("../../../text.txt");
+            string[] outputLines = new string[inputLines.Length];
+
+            int counter = 1;
+
+            foreach (string line in inputLines)
+            {
+                int lettersCounter = 0;
+                int punctuationsCounter = 0;
+
+                foreach (char character in line)
+                {
+                    if (char.IsLetter(character))
+                    {
+                        lettersCounter++;
+                    }
+                    else if (char.IsPunctuation(character))
+                    {
+                        punctuationsCounter++;
+                    }
+                }
+
+                outputLines[counter - 1] = $"Line {counter}: {inputLines[counter - 1]} ({lettersCounter})({punctuationsCounter})";
+
+                counter++;
+            }
+
+            File.WriteAllLines("../../../output.txt", outputLines);
+        }
+    }
+}
