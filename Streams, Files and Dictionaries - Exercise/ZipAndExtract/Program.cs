1bda48f2ca23764dc26adeebfc351a9503b6efa4
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-01 15:20:02 +0000

    Add ZipAndExtract exercise

---
+++ Streams, Files and Dictionaries - Exercise/ZipAndExtract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string startPath = @"../../../File";
+        string zipPath = @"../../../zipFile.zip";
+        string extractPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        ZipFile.CreateFromDirectory(startPath, zipPath);
+
+        ZipFile.ExtractToDirectory(zipPath, extractPath);
+    }
+}
