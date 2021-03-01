52de1b7413341943a61fc37d01c1c54684d0acf0
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-01 11:41:06 +0000

    Add CopyBinaryFile exercise

---
+++ Streams, Files and Dictionaries - Exercise/CopyBinaryFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream input = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (FileStream output = new FileStream("../../../output.png", FileMode.Create))
+                {
+                    while (input.Position < input.Length)
+                    {
+                        byte[] buffer = new byte[4096];
+
+                        input.Read(buffer, 0, buffer.Length);
+
+                        output.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}
