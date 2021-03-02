9891acfedeccc420cb002c5040a2aad9380efdd7
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-02 16:48:16 +0000

    Add Functional Programming - Lab folder and SortEvenNumbers exercise

---
+++ Functional Programming - Lab/SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[] output = input.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(String.Join(", ", output)); ;
+        }
+    }
+}
