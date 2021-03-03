8c3abae6b4a56b927af89b4c7951fbe9d6bb600a
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-03 08:03:08 +0000

    Add SumNumbers exercise

---
+++ Functional Programming - Lab/SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = x => int.Parse(x);
+
+            int[] input = Console.ReadLine().Split(", ").Select(parser).ToArray();
+
+            Func<int[], int> summing = x => x.Sum();
+            Func<int[], int> counting = x => x.Length;
+
+            Console.WriteLine(counting(input));
+            Console.WriteLine(summing(input));
+        }
+    }
+}
