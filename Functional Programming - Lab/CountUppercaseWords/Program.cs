97875b9381c9c559e21da2c1cd06cc2e975c98ae
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-03 09:31:03 +0000

    Add CountUppercaseWords exercise

---
+++ Functional Programming - Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputWords = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isUpper = x => char.IsUpper(x[0]);
+
+            foreach (string word in inputWords)
+            {
+                if (isUpper(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
