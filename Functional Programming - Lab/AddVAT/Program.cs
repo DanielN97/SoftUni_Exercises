871d5dc5e60e29a5f688b05bf43235675d5c8acd
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-05 12:54:22 +0000

    Add AddVAT exercise

---
+++ Functional Programming - Lab/AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Func<double, double> vatAdder = x => x * 1.2;
+
+            foreach (double number in input)
+            {
+                Console.WriteLine($"{vatAdder(number):f2}");
+            }
+        }
+    }
+}
