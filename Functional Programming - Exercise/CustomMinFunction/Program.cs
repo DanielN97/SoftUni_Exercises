8ebad6826813ce3eeb7b93516bc2240aa777fc75
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-09 18:50:54 +0000

    Add CustomMinFunction exercise

---
+++ Functional Programming - Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> minValue = x =>
+            {
+                int minNumber = int.MaxValue;
+
+                foreach (int number in input)
+                {
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                }
+
+                return minNumber;
+            };
+
+            Console.WriteLine(minValue(input));
+        }
+    }
+}
