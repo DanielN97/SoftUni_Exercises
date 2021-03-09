d1249f434436604109a7ad8ff3520e49b28513f5
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-09 16:34:44 +0000

    Add KnightsOfHonor exercise

---
+++ Functional Programming - Exercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Action<string> sirAppender = x => Console.WriteLine($"Sir {x}");
+
+            Action<string[]> printer = x =>
+            {
+                foreach (string name in input)
+                {
+                    sirAppender(name);
+                }
+            };
+
+            printer(input);
+        }
+    }
+}
