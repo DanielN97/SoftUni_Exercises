ae85f3d68fdd8b7f4ee9f0d70be6e9fe8611af8a
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-09 23:32:26 +0000

    Add AppliedArithmetics exercise

---
+++ Functional Programming - Exercise/AppliedArithmetics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                Action<int[]> printing = x => Console.WriteLine(String.Join(" ", numbers));
+
+                if (command == "print")
+                {
+                    printing(numbers);
+                }
+                else
+                {
+                    Func<int, int> action = GetAction(command);
+                    numbers = numbers.Select(action).ToArray();
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static Func<int, int> GetAction(string command)
+        {
+            Func<int, int> actions = x => x;
+
+            if (command == "add")
+            {
+                actions = x => x + 1;
+            }
+            else if (command == "multiply")
+            {
+                actions = x => x * 2;
+            }
+            else if (command == "subtract")
+            {
+                actions = x => x - 1;
+            }
+
+            return actions;
+        }
+    }
+}
