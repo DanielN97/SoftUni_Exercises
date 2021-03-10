c3de3ce324085bd3851b72c8dc9dabb5efccacf7
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-10 12:51:53 +0000

    AddCustomComparator exercise

---
+++ Functional Programming - Exercise/CustomComparator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (a, b) =>
+            {
+                if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return a.CompareTo(b);
+            });
+
+            Console.Write(String.Join(" ", numbers));
+        }
+    }
+}
