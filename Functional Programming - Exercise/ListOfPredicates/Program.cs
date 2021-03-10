50021f02f188495e6dc95728b2e308599d1b0f89
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-10 17:42:47 +0000

    Add ListOfPredicates exercise

---
+++ Functional Programming - Exercise/ListOfPredicates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endRange = int.Parse(Console.ReadLine());
+
+            int[] numbers = Enumerable.Range(1, endRange).ToArray();
+
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, bool> filter = (x, divider) => x % divider == 0;
+
+            foreach (int number in numbers)
+            {
+                if (dividers.All(d => filter(number, d)))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
