f4e08c9f03b4a0642003240dcedb3d37cc2d0318
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-10 11:19:19 +0000

    Add PredicateForNames exercise

---
+++ Functional Programming - Exercise/PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> filterForNames = (x, length) => x.Length <= length;
+
+            string[] filteredNames = names.Where(name => filterForNames(name, nameLength)).ToArray();
+
+            Console.WriteLine(String.Join(Environment.NewLine, filteredNames));
+        }
+    }
+}
