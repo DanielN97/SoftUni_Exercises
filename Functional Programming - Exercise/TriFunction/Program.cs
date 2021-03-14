f14354b2c3d6274dd94e9d8dc4c453de79c5211d
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-14 10:37:12 +0000

    Add TriFunction exercise

---
+++ Functional Programming - Exercise/TriFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> predicate = (string name, int x) =>
+            {
+                int sum = 0;
+
+                foreach (char character in name)
+                {
+                    sum += (int)character;
+                }
+
+                return sum >= x;
+            };
+
+            Console.WriteLine(names.First(x => predicate(x, number)));
+        }
+    }
+}
