2b5a9bc5fdd53050755ebff2ba71d12856096c4a
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-03-14 19:52:19 +0000

    Add ImplementingLinkedList folder and CustomDoublyLinkedList exercise

---
+++ Implementing Linked List/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                newHead.NextNode = head;
+                head.PreviousNode = newHead;
+                head = newHead;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                newTail.PreviousNode = tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+
+            Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int firstElement = head.Value;
+            head = head.NextNode;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+            else
+            {
+                head.PreviousNode = null;
+            }
+
+            Count--;
+
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int lastElement = tail.Value;
+            tail = tail.PreviousNode;
+
+            if (tail == null)
+            {
+                head = null;
+            }
+            else
+            {
+                tail.NextNode = null;
+            }
+
+            Count--;
+
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+
+            int index = 0;
+
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                array[index] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+
+                index++;
+            }
+
+            return array;
+        }
+    }
+}
