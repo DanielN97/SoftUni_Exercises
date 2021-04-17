90ca8875f94a98825ad252663b4e768c72762be6
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-04-17 17:50:04 +0100

    Add Reflection and Attributes - Lab folder and Stealer exercise

---
+++ Reflection and Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, params string[] fields)
+        {
+            Type type = Type.GetType(name);
+
+            FieldInfo[] allFields = type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(type);
+
+            sb.AppendLine($"Class under investigation: {name}");  
+
+
+            foreach (var field in allFields.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{field} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
