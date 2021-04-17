1af5af68e76ae72fd16f24c6a9b8bd8be8df1a5f
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-04-17 21:59:36 +0100

    Update AuthorProblem exercise

---
+++ Reflection and Attributes - Lab/AuthorProblem/Tracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type classType = typeof(StartUp);
+
+            MethodInfo[] methodsInfo = classType.GetMethods(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (MethodInfo method in methodsInfo)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
