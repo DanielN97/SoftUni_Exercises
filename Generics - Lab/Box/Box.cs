7c8e8f824766fe09b90695362f8c8b30b136a97f
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-04-28 08:16:25 +0100

    Add Generics - Lab folder and first two exercises

---
+++ Generics - Lab/Box/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+
+            Count++;
+        }
+
+        public T Remove()
+        {
+            T itemToRemove = default;
+
+            if (Count > 0)
+            {
+                itemToRemove = list[Count - 1];
+
+                list.Remove(itemToRemove);
+
+                Count--;
+            }
+
+            return itemToRemove;
+        }
+    }
+}
