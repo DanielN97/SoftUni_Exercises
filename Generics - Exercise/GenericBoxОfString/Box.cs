202baba0e85cb9e38d7cfda3d42995ee48a79fc8
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-04-30 17:40:45 +0100

    Add Generics - Exercise folder and GenericBoxОfString exercise

---
+++ Generics - Exercise/GenericBoxОfString/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxОfString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {Value}";
+        }
+    }
+}
