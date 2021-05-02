887b4440a27e3fa1ddd9242e508be4b94185798e
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-05-02 10:20:57 +0100

    Add GenericSwapMethodInteger exercise

---
+++ Generics - Exercise/GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{typeof(T).FullName}: {Value}";
+        }
+    }
+}
---
+++ Generics - Exercise/GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<int>> list = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+
+                list.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SwapTwoElements(list, indexes[0], indexes[1]);
+
+            foreach (Box<int> currentBox in list)
+            {
+                Console.WriteLine(currentBox);
+            }
+        }
+
+        private static void SwapTwoElements<T>(List<Box<T>> list, int firstIndex, int secondIndex)
+        {
+            Box<T> firstElement = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = firstElement;
+        }
+    }
+}
