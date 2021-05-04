1da2f82e22bffea6a088be2f99b6d332dbcf5945
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-05-04 08:35:59 +0100

    Add GenericCountMethodDouble exercise

---
+++ Generics - Exercise/GenericCountMethodDouble/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<double>> list = new List<Box<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<double> box = new Box<double>(double.Parse(Console.ReadLine()));
+
+                list.Add(box);
+            }
+
+            Box<double> itemToCompare = new Box<double>(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine(GetGreaterThanCount(list, itemToCompare));
+        }
+
+        private static int GetGreaterThanCount<T>(List<Box<T>> boxes, Box<T> box)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (Box<T> item in boxes)
+            {
+                if (item.Value.CompareTo(box.Value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
