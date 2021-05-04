2bfaec71cdca4b16dfd9885c255ad60b207a8341
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-05-04 08:28:42 +0100

    Add GenericCountMethodString exercise

---
+++ Generics - Exercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> list = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+
+                list.Add(box);
+            }
+
+            Box<string> itemToCompare = new Box<string>(Console.ReadLine());
+
+            Console.WriteLine(GetGreaterThanCount(list, itemToCompare));
+        }
+
+        private static int GetGreaterThanCount<T>(List<Box<T>> boxes, Box<T> box)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (Box<T> item in boxes)
+            {
+                if (item.Value.CompareTo(box.Value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
