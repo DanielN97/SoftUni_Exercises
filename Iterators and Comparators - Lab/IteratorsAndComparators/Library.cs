33cbb322a47c680de6e1e07401f215c6a9852dc1
Daniel Nenov <daniel.s.nenov@gmail.com>
2021-06-02 16:59:25 +0100

    Add Iterators and Comparators - Lab folder and all exercises

---
+++ Iterators and Comparators - Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            IComparer<Book> comparer = new BookComparator();
+
+            this.books = new SortedSet<Book>(books, comparer);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Lab/IteratorsAndComparators/LibraryIterator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            Reset();
+            this.books = new List<Book>(books);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            return ++currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+    }
+}
